9207b8e24e14f031dfccde7af37a7ef5103b075b
BELADEL ILYES ABDELRAZAK <ia.beladel@gmail.com>
2022-04-19 02:01:50 +0100

    Update rates.php

--- src/service/rates.php
+++ src/service/rates.php
@@ -4,24 +4,36 @@ declare(strict_types=1);
 
 namespace CommissionFees\Service;
 
+require_once __DIR__.'/../../vendor/autoload.php';
+
 date_default_timezone_set('UTC');
 
-class Rates
+class Rates extends Logs
 {
-    private $error_description = '';
-    private $api_end_point = 'https://developers.paysera.com/tasks/api/currency-exchange-rates';
     private $last_check_time = 0;
-    private $cache_time = 600; // 10 Minutes
+    private $api_end_point = 'https://developers.paysera.com/tasks/api/currency-exchange-rates';
+
+    // 10 Minutes
+    private $cache_time = 600;
+
+    /**
+     * @var array
+     */
+    private static $currencies = [];
 
     /**
      * Set the API end point to get rates.
      *
-     * @param string $api_end_point The API url to get rates (optional)
+     * @param array $api_end_point The API url to get rates (optional)
      */
-    public function __construct(string $api_end_point = '')
+    public function __construct(array $options = [])
     {
-        if (!empty($api_end_point)) {
-            $this->api_end_point = $api_end_point;
+        if (isset($options['rates_api_url'])) {
+            $this->api_end_point = $options['rates_api_url'];
+        }
+
+        if (isset($options['currency_precision'])) {
+            $this->currencies = $options['currency_precision'];
         }
     }
 
@@ -32,7 +44,7 @@ class Rates
      *
      * @return float The rate of currency on success, 0 or -1 on error
      */
-    public function getRateOf(string $currency): float
+    public function getRateOf(string $currency, int $precision = 4): float
     {
         $rates = self::getRates();
         if (empty($rates)) {
@@ -40,10 +52,10 @@ class Rates
         }
 
         if (isset($rates[$currency])) {
-            return $rates[$currency];
+            return self::getNumberPrecision($rates[$currency], $precision);
         }
 
-        $this->error_description = 'Currency symbol couldn\'t be found.';
+        parent::printDebugMessage('Currency symbol couldn\'t be found.');
 
         return 0;
     }
@@ -60,14 +72,14 @@ class Rates
         }
 
         if ($this->rates === false) {
-            $this->error_description = 'End point API couldn\'t be fetched.';
+            parent::printDebugMessage('End point API couldn\'t be fetched.');
             // return 0;
             return [];
         }
 
         $this->rates = json_decode($this->rates, true);
         if ($this->rates === null) {
-            $this->error_description = 'Invalid JSON format';
+            parent::printDebugMessage('Invalid JSON format.');
             // return -1;
             return [];
         }
@@ -76,18 +88,47 @@ class Rates
             return $this->rates['rates'];
         }
 
-        $this->error_description = 'Unknown error, may be the format is changed.';
+        parent::printDebugMessage('Unknown error, may be the format is changed.');
         // return -2;
         return [];
     }
 
     /**
-     * Get the text description of the last error.
+     * Get the rate of currency based on EUR.
      *
-     * @return string A description for last error occurred
+     * @param string $currency Currency symbol
+     *
+     * @return float The rate of currency on success, 0 or -1 on error
      */
-    public function getLastError()
+    public static function getNumberPrecision(float $amount, int $precision): float
     {
-        return $this->error_description;
+        $num = ($amount * ($p = pow(10, $precision))) / $p;
+
+        return $num;
+    }
+
+    /**
+     * Get the precision of currency.
+     *
+     * @param string $currency Currency symbol
+     *
+     * @return int The precision of currency on success, or a default value if it is doesn't exist
+     */
+    public static function getCurrencyPrecision(string $currency): int
+    {
+        $currencies = self::$currencies;
+        if (empty(self::$currencies)) {
+            $currencies = [
+                'EUR' => 2,
+                'USD' => 4,
+                'JPY' => 2,
+            ];
+        }
+
+        if (isset($currencies[$currency])) {
+            return $currencies[$currency];
+        }
+
+        return 4;
     }
 }
